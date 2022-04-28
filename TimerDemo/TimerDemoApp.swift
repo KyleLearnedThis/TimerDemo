7f267ebe86457041875990beb1df05f83e70d3f6
KyleLearnedThis <kyle.learned.this@gmail.com>
2022-04-28 15:59:53 -0700

    Initial Commit

---
+++ TimerDemo/TimerDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimerDemoApp.swift
+//  TimerDemo
+//
+//  Created by KyleLearnedThis on 4/28/22.
+//
+
+import SwiftUI
+
+@main
+struct TimerDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
